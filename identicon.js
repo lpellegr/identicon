f34965d516daf0e15f2b83710db13db0927bb3e8
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2013-09-23 21:21:31 +0200

    Replace tabs with spaces

--- identicon.js
+++ identicon.js
@@ -5,20 +5,20 @@
  */
 Identicon.PATCHES = [
     [0, 4, 24, 20],
-	[0, 4, 20],
-	[2, 24, 20],
-	[0, 2, 20, 22],
-	[2, 14, 22, 10],
-	[0, 14, 24, 22],
-	[2, 24, 22, 13, 11, 22, 20],
-	[0, 14, 22],
-	[6, 8, 18, 16],
-	[4, 20, 10, 12, 2],
-	[0, 2, 12, 10],
-	[10, 14, 22],
-	[20, 12, 24],
-	[10, 2, 12],
-	[0, 2, 10]
+    [0, 4, 20],
+    [2, 24, 20],
+    [0, 2, 20, 22],
+    [2, 14, 22, 10],
+    [0, 14, 24, 22],
+    [2, 24, 22, 13, 11, 22, 20],
+    [0, 14, 22],
+    [6, 8, 18, 16],
+    [4, 20, 10, 12, 2],
+    [0, 2, 12, 10],
+    [10, 14, 22],
+    [20, 12, 24],
+    [10, 2, 12],
+    [0, 2, 10]
 ];
 
 Identicon.PATCH_GRIDS = 5;
@@ -26,136 +26,134 @@ Identicon.PATCH_TYPE_CENTER = [0, 4, 8, 15];
 Identicon.DEFAULT_BACKGROUND_COLOR = "white";
 
 Identicon.fromId = function(elementId, backgroundColor) {
-	return Identicon.fromDOMElement(document.getElementById(elementId), backgroundColor);
+    return Identicon.fromDOMElement(document.getElementById(elementId), backgroundColor);
 }
 
 Identicon.fromDOMElement = function(element, backgroundColor) {
-	if (!backgroundColor) {
-		backgroundColor = Identicon.DEFAULT_BACKGROUND_COLOR;
-	}
+    if (!backgroundColor) {
+        backgroundColor = Identicon.DEFAULT_BACKGROUND_COLOR;
+    }
 
-	return new Identicon(element, backgroundColor);
+    return new Identicon(element, backgroundColor);
 }
 
+/**
+ * An identicon.
+ * @constructor
+ */
 function Identicon(element, backgroundColor) {
-	this.element = element;
-	this.backgroundColor = backgroundColor;
-	this.code = element.getAttribute("data-value")
-		.hashCode();
-	this.size = element.clientWidth;
+    var element = element;
+    var backgroundColor = backgroundColor;
+    var code = element.getAttribute("data-value")
+        .hashCode();
+    var size = element.clientWidth;
 
-	//console.info("Identicon hash value is '" + this.code + "'");
-	//console.info("Canvas width is '" + this.size + "'")
+    this.draw = function() {
+        var middlePatchNumber = Identicon.PATCH_TYPE_CENTER[code & 3];
+        var middleInvert = ((code >> 2) & 1) != 0;
+        var cornerType = (code >> 3) & 15;
+        var cornerInvert = ((code >> 7) & 1) != 0;
+        var cornerTurn = (code >> 8) & 3;
+        var sideType = (code >> 10) & 15;
+        var sideInvert = ((code >> 14) & 1) != 0;
+        var sideTurn = (code >> 15) & 3;
+        var blue = (code >> 16) & 31;
+        var green = (code >> 21) & 31;
+        var red = (code >> 27) & 31;
+        var color = "rgb(" + (red << 3) + "," + (green << 3) + "," + (blue << 3) + ")";
 
-	this.draw = function() {
-		var middlePatchNumber = Identicon.PATCH_TYPE_CENTER[this.code & 3];
-		var middleInvert = ((this.code >> 2) & 1) != 0;
-		var cornerType = (this.code >> 3) & 15;
-		var cornerInvert = ((this.code >> 7) & 1) != 0;
-		var cornerTurn = (this.code >> 8) & 3;
-		var sideType = (this.code >> 10) & 15;
-		var sideInvert = ((this.code >> 14) & 1) != 0;
-		var sideTurn = (this.code >> 15) & 3;
-		var blue = (this.code >> 16) & 31;
-		var green = (this.code >> 21) & 31;
-		var red = (this.code >> 27) & 31;
-		var color = "rgb(" + (red << 3) + "," + (green << 3) + "," + (blue << 3) + ")";
+        var paper = Raphael(element, size, size);
+        var paperBackground = paper.rect(0, 0, paper.width, paper.height);
+        paperBackground.attr({
+            "fill": backgroundColor,
+            "stroke": "none"
+        });
 
-		var paper = Raphael(this.element, this.size, this.size);
-		var paperBackground = paper.rect(0, 0, paper.width, paper.height);
-		paperBackground.attr({
-			"fill": this.backgroundColor,
-			"stroke": "none"
-		});
+        var patchSize = size / 3;
+        var patchSize2 = patchSize * 2;
 
-		var patchSize = this.size / 3;
-		var patchSize2 = patchSize * 2;
+        // middle patch
+        drawPatch(paper, middlePatchNumber, patchSize, patchSize, patchSize, backgroundColor, color, 0, middleInvert);
 
-		// middle patch
-		drawPatch(paper, middlePatchNumber, patchSize, patchSize, patchSize, this.backgroundColor, color, 0, middleInvert);
+        // side patches (N,E,S,O)
+        drawPatch(paper, sideType, patchSize, patchSize, 0, backgroundColor, color, sideTurn++, sideInvert);
+        drawPatch(paper, sideType, patchSize, patchSize2, patchSize, backgroundColor, color, sideTurn++, sideInvert);
+        drawPatch(paper, sideType, patchSize, patchSize, patchSize2, backgroundColor, color, sideTurn++, sideInvert);
+        drawPatch(paper, sideType, patchSize, 0, patchSize, backgroundColor, color, sideTurn++, sideInvert);
 
-		// side patches (N,E,S,O)
-		drawPatch(paper, sideType, patchSize, patchSize, 0, this.backgroundColor, color, sideTurn++, sideInvert);
-		drawPatch(paper, sideType, patchSize, patchSize2, patchSize, this.backgroundColor, color, sideTurn++, sideInvert);
-		drawPatch(paper, sideType, patchSize, patchSize, patchSize2, this.backgroundColor, color, sideTurn++, sideInvert);
-		drawPatch(paper, sideType, patchSize, 0, patchSize, this.backgroundColor, color, sideTurn++, sideInvert);
+        // corner patches
+        drawPatch(paper, cornerType, patchSize, 0, 0, backgroundColor, color, cornerTurn++, cornerInvert);
+        drawPatch(paper, cornerType, patchSize, patchSize2, 0, backgroundColor, color, cornerTurn++, cornerInvert);
+        drawPatch(paper, cornerType, patchSize, patchSize2, patchSize2, backgroundColor, color, cornerTurn++, cornerInvert);
+        drawPatch(paper, cornerType, patchSize, 0, patchSize2, backgroundColor, color, cornerTurn++, cornerInvert);
+    };
 
-		// corner patches
-		drawPatch(paper, cornerType, patchSize, 0, 0, this.backgroundColor, color, cornerTurn++, cornerInvert);
-		drawPatch(paper, cornerType, patchSize, patchSize2, 0, this.backgroundColor, color, cornerTurn++, cornerInvert);
-		drawPatch(paper, cornerType, patchSize, patchSize2, patchSize2, this.backgroundColor, color, cornerTurn++, cornerInvert);
-		drawPatch(paper, cornerType, patchSize, 0, patchSize2, this.backgroundColor, color, cornerTurn++, cornerInvert);
-	};
+    function createPath(patch, x, y, scale, offset) {
+        var mx = (patch[0] % Identicon.PATCH_GRIDS * scale - offset);
+        mx += x + offset;
 
-	function createPath(patch, x, y, scale, offset) {
-		//console.log("patch=" + patch +  ", x=" +  x + ", y=" + y + ", scale=" + scale + ", offset=" +  offset);
+        var my = Math.floor(patch[0] / Identicon.PATCH_GRIDS) * scale - offset;
+        my += y + offset;
 
-		var mx = (patch[0] % Identicon.PATCH_GRIDS * scale - offset);
-		mx += x + offset;
+        var result = "M" + mx + "," + my;
 
-		var my = Math.floor(patch[0] / Identicon.PATCH_GRIDS) * scale - offset;
-		my += y + offset;
+        for (var i = 1; i < patch.length; i++) {
+            var v = patch[i];
+            var vx = v % Identicon.PATCH_GRIDS * scale - offset;
+            vx += x + offset;
+            var vy = Math.floor(v / Identicon.PATCH_GRIDS) * scale - offset;
+            vy += y + offset;
 
-		var result = "M" + mx + "," + my;
+            result += "L" + vx + "," + vy;
+        }
 
-		for (var i = 1; i < patch.length; i++) {
-			var v = patch[i];
-			var vx = v % Identicon.PATCH_GRIDS * scale - offset;
-			vx += x + offset;
-			var vy = Math.floor(v / Identicon.PATCH_GRIDS) * scale - offset;
-			vy += y + offset;
+        result += "Z";
+        return result;
+    }
 
-			result += "L" + vx + "," + vy;
-		}
+    function drawPatch(paper, patch, size, x, y, backgroundColor, foregroundColor, turn, invert) {
+        patch %= Identicon.PATCHES.length;
+        turn %= 4;
 
-		result += "Z";
-		return result;
-	}
+        if (patch == 15) {
+            invert = !invert;
+        }
 
-	function drawPatch(paper, patch, size, x, y, backgroundColor, foregroundColor, turn, invert) {
-		patch %= Identicon.PATCHES.length;
-		turn %= 4;
+        if (invert) {
+            rectangle = paper.rect(x, y, size, size);
+            rectangle.attr({
+                "fill": foregroundColor,
+                "stroke": "none"
+            });
+        }
 
-		if (patch == 15) {
-			invert = !invert;
-		}
+        var offset = size / 2;
+        var scale = size / 4;
 
-		if (invert) {
-			rectangle = paper.rect(x, y, size, size);
-			rectangle.attr({
-				"fill": foregroundColor,
-				"stroke": "none"
-			});
-		}
+        var path = createPath(Identicon.PATCHES[patch], x, y, scale, offset);
 
-		var offset = size / 2;
-		var scale = size / 4;
-
-		var path = createPath(Identicon.PATCHES[patch], x, y, scale, offset);
-		//console.log(path);
-
-		var shape = paper.path(path);
-		shape.rotate(turn * 90);
-		shape.attr({
-			"stroke": "none",
-			"stroke-width": "1",
-			"fill": invert ? backgroundColor : foregroundColor
-		});
-	};
+        var shape = paper.path(path);
+        shape.rotate(turn * 90);
+        shape.attr({
+            "stroke": "none",
+            "stroke-width": "1",
+            "fill": invert ? backgroundColor : foregroundColor
+        });
+    };
 }
 
 String.prototype.hashCode = function() {
-	var hash = 0;
+    var hash = 0;
 
-	if (this.length == 0) {
-		return hash;
-	}
+    if (this.length == 0) {
+        return hash;
+    }
 
-	for (i = 0; i < this.length; i++) {
-		character = this.charCodeAt(i);
-		hash = ((hash << 5) - hash) + character;
-		hash = hash & hash;
-	}
+    for (i = 0; i < this.length; i++) {
+        character = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + character;
+        hash = hash & hash;
+    }
 
-	return hash;
+    return hash;
 }
